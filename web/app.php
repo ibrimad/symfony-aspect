c7e147ee1fe9f75747fadc736445c4019e672280
Alexander Lissachenko <lisachenko.it@gmail.com>
2012-12-24 00:22:51 +0400

    Enable demo bundle for production mode and adjust aspect to work in production mode

--- web/app.php
+++ web/app.php
@@ -3,7 +3,28 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+//$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+
+include __DIR__.'/../vendor/lisachenko/go-aop-php/src/Go/Core/AspectKernel.php';
+include __DIR__.'/../app/DemoAspectKernel.php';
+
+// Initialize aspect container
+$aspectKernel = DemoAspectKernel::getInstance();
+$aspectKernel->init(array(
+    'autoload' => array(
+        'Aspect'           => __DIR__.'/../src/Aspect',
+        'Go'               => __DIR__.'/../vendor/lisachenko/go-aop-php/src/',
+        'TokenReflection'  => __DIR__.'/../vendor/andrewsville/php-token-reflection/',
+        'Doctrine\\Common' => __DIR__.'/../vendor/doctrine/common/lib/'
+    ),
+    'includePaths' => array(
+        __DIR__.'/../src/',
+
+    ),
+    'cacheDir' => __DIR__ .'/../app/cache/aspect',
+    'appDir'   => __DIR__.'/../',
+    'debug' => false,
+));
 
 // Use APC for autoloading to improve performance.
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
